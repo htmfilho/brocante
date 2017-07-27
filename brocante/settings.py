a51c5d4fbc3622e78ffea21f210c24b65d867b3b
Hildeberto Mendonca <me@hildeberto.com>
2017-07-27 17:30:51 +0200

    Set PostgreSQL as database

--- brocante/settings.py
+++ brocante/settings.py
@@ -76,13 +76,8 @@ WSGI_APPLICATION = 'brocante.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DATABASES = {'default': dj_database_url.config(conn_max_age=600,
+                                               default='postgres://brocante_usr:secret@localhost:5432/brocante')}
 
 AUTH_PASSWORD_VALIDATORS = [
     {
