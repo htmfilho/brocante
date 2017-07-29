983f9b3f1dc0df333e714845245b1547c4a75526
Hildeberto Mendonca <me@hildeberto.com>
2017-07-29 17:20:04 +0200

    Added registration date and status

--- inscription/models.py
+++ inscription/models.py
@@ -1,13 +1,20 @@
 from django.db import models
 from django.contrib import admin
+from django.utils.translation import ugettext_lazy as _
 
 class InscriptionAdmin(admin.ModelAdmin):
-    list_display = ('first_name', 'last_name', 'address', 'email', 'phone', 'number_places', 'desired_place')
-    fieldsets = ((None, {'fields': ('last_name', 'first_name', 'address', 'email', 'phone', 'number_places', 'desired_place')}),)
+    list_display = ('first_name', 'last_name', 'status', 'address', 'email', 'phone', 'number_places', 'desired_place', 'registered')
+    list_filter = ('status',)
+    fieldsets = ((None, {'fields': ('last_name', 'first_name', 'status', 'address', 'email', 'phone', 'number_places', 'desired_place')}),)
     search_fields = ['first_name', 'last_name', 'address', 'email', 'phone']
-    
+
 
 class Inscription(models.Model):
+    STATUS_CHOICES = (
+        ('NOT_CONFIRMED', _('Not Confirmed')),
+        ('CONFIRMED', _('Confirmed')),
+        ('WAITING_LIST', _('Waiting List')))
+
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     address = models.TextField()
@@ -15,3 +22,5 @@ class Inscription(models.Model):
     phone = models.CharField(max_length=20)
     number_places = models.IntegerField()
     desired_place = models.TextField()
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='NOT_CONFIRMED')
+    registered = models.DateTimeField(null=True, auto_now=True)
