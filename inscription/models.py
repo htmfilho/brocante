2886a7277a8ef3acb0e5f34f69066f65a44a8b3a
Hildeberto Mendonca <me@hildeberto.com>
2017-07-29 16:55:39 +0200

    Added search fields

--- inscription/models.py
+++ inscription/models.py
@@ -4,7 +4,8 @@ from django.contrib import admin
 class InscriptionAdmin(admin.ModelAdmin):
     list_display = ('first_name', 'last_name', 'address', 'email', 'phone', 'number_places', 'desired_place')
     fieldsets = ((None, {'fields': ('last_name', 'first_name', 'address', 'email', 'phone', 'number_places', 'desired_place')}),)
-
+    search_fields = ['first_name', 'last_name', 'address', 'email', 'phone']
+    
 
 class Inscription(models.Model):
     first_name = models.CharField(max_length=30)
