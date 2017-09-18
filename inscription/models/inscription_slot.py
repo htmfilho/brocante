8370105161ec6c4fec5cf882e4173dcc4ba615eb
Hildeberto Mendonca <me@hildeberto.com>
2017-09-18 21:49:07 +0200

    Show slots of each inscription in the list page.

--- inscription/models/inscription_slot.py
+++ inscription/models/inscription_slot.py
@@ -8,7 +8,7 @@ from inscription.utils import post_officer
 
 
 class InscriptionSlotAdmin(admin.ModelAdmin):
-    list_display = ('inscription', 'slot')
+    list_display = ('slot', 'inscription')
     fieldsets = ((None, {'fields': ('inscription', 'slot')}),)
     raw_id_fields = ('inscription',)
     actions = ['send_instructions']
@@ -20,13 +20,10 @@ class InscriptionSlotAdmin(admin.ModelAdmin):
 
 class InscriptionSlot(models.Model):
     inscription = models.ForeignKey('Inscription', verbose_name=_("Inscription"))
-    slot = models.ForeignKey('Slot', verbose_name=_("Slot"))
-
-    class Meta:
-        unique_together = ('inscription', 'slot')
+    slot = models.OneToOneField('Slot', verbose_name=_("Slot"))
 
     def __str__(self):
-        return "{} - {}".format(self.inscription, self.slot)
+        return "{} - {}".format(self.slot, self.inscription)
 
 
 def find_by_inscription(inscription):
@@ -45,7 +42,7 @@ def send_instructions_emails(inscription):
         inscription_slot = inscription_slots.first()
 
         if slots_quant > 1:
-            slots_list = ", ".join([insc_slot.slot.identification for insc_slot in inscription_slots])
+            slots_list = _get_list_slots(inscription_slots)
             slots = "{}: <strong>{}</strong>".format(_('The numbers of your slots are'), slots_list)
             location = "{}: <strong>{}</strong>".format(_('The location of your slots are'), inscription_slot.slot.location)
         else:
@@ -57,3 +54,12 @@ def send_instructions_emails(inscription):
         recipients = [inscription.email]
         post_officer.send_message(recipients, subject, template.render(context), message_history.INSTRUCTIONS,
                                   html_message=template_html.render(context))
+
+
+def _get_list_slots(inscription_slots):
+    return ", ".join([insc_slot.slot.identification for insc_slot in inscription_slots])
+
+
+def get_list_slots(inscription):
+    inscription_slots = find_by_inscription(inscription)
+    return _get_list_slots(inscription_slots)
--- inscription/views/inscriptions.py
+++ inscription/views/inscriptions.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import user_passes_test
 
-from inscription.models import inscription
+from inscription.models import inscription, inscription_slot as inscr_slot
 
 
 @user_passes_test(lambda u: u.is_staff)
 def inscriptions_view(request):
     total_demanded_places = inscription.get_total_demanded_places()
     total_confirmed_places = inscription.get_total_confirmed_places()
-    inscriptions = inscription.find_confirmed_inscriptions()
+    inscriptions = [{"last_name": inscr.last_name,
+                     "first_name": inscr.first_name,
+                     "address": inscr.address,
+                     "phone": inscr.phone,
+                     "number_places": inscr.number_places,
+                     "desired_place": inscr.desired_place,
+                     "slots": inscr_slot.get_list_slots(inscr)} for inscr in inscription.find_confirmed_inscriptions()]
     return render(request, "inscriptions.html", locals())
