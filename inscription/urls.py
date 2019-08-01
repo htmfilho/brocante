5b324a5160bf0d41dcbe703c44b04d89bc5cb2d3
Hildeberto Mendonca <me@hildeberto.com>
2019-07-31 23:12:15 -0400

    Fixed email url

--- inscription/urls.py
+++ inscription/urls.py
@@ -3,7 +3,7 @@ from inscription.views import inscription_form, inscriptions, places
 
 urlpatterns = [
     url(r'^$', inscription_form.inscription, name='inscription'),
-    url(r'^email$', inscription_form.email_exists, name='email_exists'),
+    url(r'email$', inscription_form.email_exists, name='email_exists'),
     url(r'submission$', inscription_form.inscription_submission, name='inscription_submission'),
     url(r'inscriptions$', inscriptions.inscriptions_view, name='inscriptions'),
     url(r'places$', places.places_view, name='places'),
